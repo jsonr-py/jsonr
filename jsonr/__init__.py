3309c93fed018afb3f2239b3639464a90cea4634
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-07 13:03:50 -0500

    Update __init__.py

--- jsonr/__init__.py
+++ jsonr/__init__.py
@@ -3,10 +3,16 @@ Create highly customizable JSON objects.
 
     - jsonr.New(auto=False, limit=None, *args, **kwargs)
 
-Run `help(jsonr.New)` or `help(jsonr.JSON)` for more information.
+Create a dataframe.
+    - jsonr.Frame(columns=[], rows=[])
+
+Run `help(jsonr.New)` or `help(jsonr.JSON)` for more information on json objects.
+
+
+Run `help(jsonr.Frame)` for more information on dataframe objects.
 '''
 
-from .core import New
+from .core import New, Frame
 
 JSON: New = New
 
@@ -20,3 +26,28 @@ if sys.version_info < (PYTHON_MINIMUM_MAJOR, PYTHON_MINIMUM_MINOR, PYTHON_MINIMU
     alert: str = f'\n\nPython {PYTHON_MINIMUM_MAJOR}.{PYTHON_MINIMUM_MINOR}.{PYTHON_MINIMUM_MICRO} or higher is required for {__name__}.\n\nCurrent Version: {str(sys.version).split("(")[0]}\n\n>>> Error Below:'
     import warnings
     warnings.warn(alert, stacklevel=2, category=RuntimeWarning)
+
+__all__:          tuple = (
+                        'New',
+                        'Frame',
+                        'JSON',
+                        'PYTHON_MINIMUM_MAJOR',
+                        'PYTHON_MINIMUM_MINOR',
+                        'PYTHON_MINIMUM_MICRO'
+)
+
+__version__:      str = '8.5.1'
+__author__:       str = 'jsonr'
+__author_email__: str = 'jsonr.py@gmail.com'
+__license__:      str = 'MIT'
+__copyright__:    str = 'Copyright (c) 2023 jsonr'
+__credits__:      str = 'jsonr'
+__maintainer__:   str = 'jsonr'
+__email__:        str = 'jsonr.py@gmail.com'
+__status__:       str = 'Development Status :: 3 - Alpha'
+__topic__:        str = 'Topic :: Software Development :: Build Tools'
+__license_type__: str = 'License :: OSI Approved :: MIT License'
+__url__:          str = 'https://github.com/jsonr-py/jsonr'
+__github__:       str = 'https://github.com/jsonr-py'
+__repo__ :        str = 'https://github.com/jsonr-py/jsonr'
+__keywords__:     str = 'jsonr json jsonr-py jsonr-python'
