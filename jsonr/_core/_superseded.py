b5da75b17819d24c8e717ee867e2f47faed85a7f
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:42:45 -0500

    Create _superseded.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/_core/_superseded.py
@@ -0,0 +1,43 @@
+import aifc
+import audioop
+import cgi
+import cgitb
+import chunk
+import crypt
+import imghdr
+import mailcap
+import msilib
+import nis
+import nntplib
+import optparse
+import ossaudiodev
+import pipes
+import sndhdr
+import spwd
+import sunau
+import telnetlib
+import uu
+import xdrlib
+
+class Superseded:
+    def __init__(self):
+        self.aifc: object = aifc
+        self.audioop: object = audioop
+        self.cgi: object = cgi
+        self.cgitb: object = cgitb
+        self.chunk: object = chunk
+        self.crypt: object = crypt
+        self.imghdr: object = imghdr
+        self.mailcap: object = mailcap
+        self.msilib: object = msilib
+        self.nis: object = nis
+        self.nntplib: object = nntplib
+        self.optparse: object = optparse
+        self.ossaudiodev: object = ossaudiodev
+        self.pipes: object = pipes
+        self.sndhdr: object = sndhdr
+        self.spwd: object = spwd
+        self.sunau: object = sunau
+        self.telnetlib: object = telnetlib
+        self.uu: object = uu
+        self.xdrlib: object = xdrlib
