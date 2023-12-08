082788ae407aeafb63ae2660902183c38eab7a7f
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:38:09 -0500

    Create _functional.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/_core/_functional.py
@@ -0,0 +1,9 @@
+import itertools
+import functools
+import operator
+
+class Functional:
+    def __init__(self):
+        self.itertools: object = itertools
+        self.functools: object = functools
+        self.operator: object = operator
