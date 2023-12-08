f7393ddc8ab4e1b596307443c439f5b3eb190e9c
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:41:53 -0500

    Create _packaging.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/_core/_packaging.py
@@ -0,0 +1,9 @@
+import ensurepip
+import venv
+import zipapp
+
+class Packaging:
+    def __init__(self):
+        self.ensurepip: object = ensurepip
+        self.venv: object = venv
+        self.zipapp: object = zipapp
