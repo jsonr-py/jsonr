9f4200177699ab16f1b44be42d93d7ba5d4bcecf
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:34:47 -0500

    Create __init__.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/_core/__init__.py
@@ -0,0 +1,103 @@
+'''
+jsonr._core
+
+jsor.core backend
+python.builtins when needed
+
+__all__: tuple = (
+    'Binary',
+    'Code',
+    'Concurrency',
+    'Cryptography',
+    'Data',
+    'dCompression',
+    'Debug',
+    'DevTools',
+    'dTypes',
+    'File',
+    'FileFormats',
+    'Functional',
+    'GUItk',
+    'InternetData',
+    'Importing',
+    'Internationalization',
+    'InternetProtocols',
+    'Language',
+    'Markup',
+    'Math',
+    'Windows',
+    'MultiMedia',
+    'Networking',
+    'OperatingSystem',
+    'Packaging',
+    'FrameWorks',
+    'Runtime',
+    'Superseded',
+    'Text',
+    'Unix'
+)
+'''
+
+from ._binary import Binary
+from ._code import Code
+from ._concurrent import Concurrency
+from ._crypto import Cryptography
+from ._data import Data
+from ._dcompress import dCompression
+from ._debug import Debug
+from ._devtools import DevTools
+from ._dtypes import dTypes
+from ._file import File
+from ._formats import FileFormats
+from ._functional import Functional
+from ._guitk import GUItk
+from ._idata import InternetData
+from ._import import Importing
+from ._international import Internationalization
+from ._iprotocols import InternetProtocols
+from ._language import Language
+from ._markup import Markup
+from ._math import Math
+from ._mswindows import Windows
+from ._multimedia import MultiMedia
+from ._networking import Networking
+from ._os import OperatingSystem
+from ._packaging import Packaging
+from ._program import FrameWorks
+from ._runtime import Runtime
+from ._superseded import Superseded
+from ._text import Text
+from ._unix import Unix
+
+__all__: tuple = (
+    'Binary',
+    'Code',
+    'Concurrency',
+    'Cryptography',
+    'Data',
+    'dCompression',
+    'Debug',
+    'DevTools',
+    'dTypes',
+    'File',
+    'FileFormats',
+    'Functional',
+    'GUItk',
+    'InternetData',
+    'Importing',
+    'Internationalization',
+    'InternetProtocols',
+    'Language',
+    'Markup',
+    'Math',
+    'Windows',
+    'MultiMedia',
+    'Networking',
+    'OperatingSystem',
+    'Packaging',
+    'FrameWorks',
+    'Runtime',
+    'Superseded',
+    'Text',
+    'Unix'
+)
