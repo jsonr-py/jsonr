29156295ee0f76fd044cbded682e61da062e99c0
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:38:28 -0500

    Create _guitk.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/_core/_guitk.py
@@ -0,0 +1,19 @@
+import tkinter
+import tkinter.colorchooser
+import tkinter.font
+import tkinter.messagebox
+import tkinter.scrolledtext
+import tkinter.dnd
+import tkinter.ttk
+import tkinter.tix
+
+class GUItk:
+    def __init__(self):
+        self.tkinter: object = tkinter
+        self.tkinter_colorchooser: object = tkinter.colorchooser
+        self.tkinter_font: object = tkinter.font
+        self.tkinter_messagebox: object = tkinter.messagebox
+        self.tkinter_scrolledtext: object = tkinter.scrolledtext
+        self.tkinter_dnd: object = tkinter.dnd
+        self.tkinter_ttk: object = tkinter.ttk
+        self.tkinter_tix: object = tkinter.tix
