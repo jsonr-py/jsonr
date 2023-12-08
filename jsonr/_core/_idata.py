a238a26a941eb98cff13c7e5b55fbf8182501970
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:38:42 -0500

    Create _idata.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/_core/_idata.py
@@ -0,0 +1,17 @@
+import email
+import json
+import mailbox
+import mimetypes
+import base64
+import binascii
+import quopri
+
+class InternetData:
+    def __init__(self):
+        self.email: object = email
+        self.json: object = json
+        self.mailbox: object = mailbox
+        self.mimetypes: object = mimetypes
+        self.base64: object = base64
+        self.binascii: object = binascii
+        self.quopri: object = quopri
