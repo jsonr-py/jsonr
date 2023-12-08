610d8a44bfb2298a9b831e0720c42fbe8a2dbc09
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:27:45 -0500

    Create __init__.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/typing/__init__.py
@@ -0,0 +1,107 @@
+'''
+jsonr.typing
+
+jsonr frontend
+
+__all__: tuple = (
+    'String',
+    'StringBuffer',
+    'JSONArray',
+    'JSONObject',
+    'JSONValue',
+    'Flowa',
+    'FlowaArray',
+    'FlowaObject',
+    'FlowaValue',
+    'Image',
+    'ImageStr',
+    'ImageObject',
+    'ImageUrl',
+    'Text',
+    'TextArray',
+    'TextObject',
+    'TextValue',
+    'Any',
+    'Callable',
+    'Dict',
+    'List',
+    'Optional',
+    'Tuple',
+    'Type',
+    'Union',
+    'cast',
+    'overload'
+)
+'''
+
+from .._typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Type,
+    Union,
+    cast,
+    overload
+)
+
+from .._typing import (
+    String,
+    StringBuffer
+)
+
+from .._typing import (
+    JSONArray,
+    JSONObject,
+    JSONValue
+)
+
+from .._typing import (
+    Flowa,
+    FlowaArray,
+    FlowaObject,
+    FlowaValue
+)
+
+from .._typing import (
+    Image,
+    ImageStr,
+    ImageObject,
+    ImageUrl,
+    Text,
+    TextArray,
+    TextObject,
+    TextValue
+)
+
+__all__: tuple = (
+    'String',
+    'StringBuffer',
+    'JSONArray',
+    'JSONObject',
+    'JSONValue',
+    'Flowa',
+    'FlowaArray',
+    'FlowaObject',
+    'FlowaValue',
+    'Image',
+    'ImageStr',
+    'ImageObject',
+    'ImageUrl',
+    'Text',
+    'TextArray',
+    'TextObject',
+    'TextValue',
+    'Any',
+    'Callable',
+    'Dict',
+    'List',
+    'Optional',
+    'Tuple',
+    'Type',
+    'Union',
+    'cast',
+    'overload'
+)
