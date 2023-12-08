5ad86d0e7d50e5b2dd7ce6e22c090f251d4567d5
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:40:39 -0500

    Create _math.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/_core/_math.py
@@ -0,0 +1,17 @@
+import math
+import numbers
+import cmath
+import decimal
+import fractions
+import random
+import statistics
+
+class Math:
+    def __init__(self):
+        self.math: object = math
+        self.numbers: object = numbers
+        self.cmath: object = cmath
+        self.decimal: object = decimal
+        self.fractions: object = fractions
+        self.random: object = random
+        self.statistics: object = statistics
