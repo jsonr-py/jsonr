26751a999e7fb35ed0db79b63327d7298615296e
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:37:08 -0500

    Create _dtypes.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/_core/_dtypes.py
@@ -0,0 +1,33 @@
+import datetime
+import zoneinfo
+import calander
+import collections
+import collections.abc
+import heapq
+import bisect
+import array
+import weakref
+import types
+import copy
+import pprint
+import reprlib
+import enum
+import graphlib
+
+class dTypes:
+    def __init__(self):
+        self.datetime: object = datetime
+        self.zoneinfo: object = zoneinfo
+        self.calander: object = calander
+        self.collections: object = collections
+        self.collections_abc: object = collections.abc
+        self.heapq: object = heapq
+        self.bisect: object = bisect
+        self.array: object = array
+        self.weakref: object = weakref
+        self.types: object = types
+        self.copy: object = copy
+        self.pprint: object = pprint
+        self.reprlib: object = reprlib
+        self.enum: object = enum
+        self.graphlib: object = graphlib
