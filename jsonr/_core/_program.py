48873052b2596ebeb1ce42a14092b3e4868b45f4
jsonr <153175025+jsonr-py@users.noreply.github.com>
2023-12-08 02:42:12 -0500

    Create _program.py
    Signed-off-by: jsonr <153175025+jsonr-py@users.noreply.github.com>

---
+++ jsonr/_core/_program.py
@@ -0,0 +1,9 @@
+import turtle
+import cmd
+import shlex
+
+class FrameWorks:
+    def __init__(self):
+        self.turtle: object = turtle
+        self.cmd: object = cmd
+        self.shlex: object = shlex
